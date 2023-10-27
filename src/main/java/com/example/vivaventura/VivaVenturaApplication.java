eb4e783886bd70342851b02b48ec71a012c4c6cd
dalamo20 <dalamo90@gmail.com>
2023-10-27 17:08:49 -0500

    Initial commit

---
+++ src/main/java/com/example/vivaventura/VivaVenturaApplication.java
@@ -0,0 +1,12 @@
+package com.example.vivaventura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaVenturaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hey");
+    }
+}
---
+++ src/test/java/com/example/vivaventura/VivaVenturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vivaventura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaVenturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
