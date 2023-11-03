1f29933495babd8a019496427cb831745f2e9b38
dalamo20 <dalamo90@gmail.com>
2023-11-03 16:26:24 -0500

    added User creation services

--- src/main/java/com/vivaventura/model/services/factory/ServiceFactory.java
+++ src/main/java/com/vivaventura/model/services/factory/ServiceFactory.java
@@ -1,10 +1,16 @@
 package com.vivaventura.model.services.factory;
 
 import com.vivaventura.model.services.loginservice.ILoginService;
+import com.vivaventura.model.services.loginservice.IUserService;
 import com.vivaventura.model.services.loginservice.LoginServiceImpl;
+import com.vivaventura.model.services.loginservice.UserServiceImpl;
 
 public class ServiceFactory {
     public ILoginService getLoginService() {
         return new LoginServiceImpl();
+    }
+
+    public IUserService getUserService() {
+        return new UserServiceImpl();
     }
 }
